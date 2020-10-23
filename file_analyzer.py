0b4705168b2e20c3ebd569ec9fed69542506559f
Manfred Madelaine <manfred.madelaine@outlook.fr>
2020-10-24 00:29:48 +0200

    :sparkles: Retrieve properties from directory's entries

--- file_analyzer.py
+++ file_analyzer.py
@@ -1,30 +1,47 @@
 import os
+import datetime
 
 
 ROOT = 'new_files'
+DATE_FORMAT = '%Y-%m-%d-%H:%M:%S'
 
+class DirEntry:
+    def __init__(self, name, path, type, size, creation_time):
+        self.name = name
+        self.path = path
+        self.type = type
+        self.size = size
+        self.creation_time = creation_time
+
+    def __str__(self):
+        date_time = datetime.datetime.fromtimestamp(self.creation_time).strftime(DATE_FORMAT)
+        txt = f'{self.name} (format: {self.type}, size: {self.size} bytes, creation date: {date_time})'
+        return txt
 
 def analyze(root):
     # print content
-    # test(root)
-    get_properties(root)
+    retreive_properties(root)
 
-     # gather infos
 
-    # include directories
 
-def test(directory_path):
-    for root, dirs, files in os.walk(directory_path):
-        for file_name in files:
-            # list += [file_name]
-            print(file_name)
-
-def get_properties(path):
+def retreive_properties(path):
+    properties = []
     with os.scandir(path) as it:
         for entry in it:
             if not entry.name.startswith('.'):
-                print(entry.name)
-                print(entry.stat())
+                type = 'dir' if entry.is_dir() else entry.name.split('.')[-1]
+
+                dir_entry = DirEntry(
+                                entry.name, 
+                                entry.path, 
+                                type, 
+                                entry.stat().st_size,
+                                entry.stat().st_ctime)
+
+                properties += [dir_entry]
+
+    [print(d) for d in properties]
+    return properties
 
 
 # -------------- Main --------------
