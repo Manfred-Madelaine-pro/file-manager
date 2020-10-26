461d2014b5a805a46f63e44657c781890fd4fea3
manfred.madelaine <manfred.madelaine@outlook.fr>
2020-10-26 22:29:53 +0100

    :books: DOC: Define guiding blocks for the modifyer

---
+++ src/structure_modifyer.py
@@ -0,0 +1,25 @@
+
+
+def structure_modifyer(structure_path):
+	# retreive file
+	# dump from json to objects
+
+	# get filters
+	# get destination
+	# apply filter
+	# save new structure file
+	pass
+
+	
+# 1 regroup by date
+	# year & month
+
+# 2 regroup files per extension typy
+	# define groups and sub groups 
+	# with the "other" group containing all the unhandled types
+
+
+def main():
+	print("hi")
+
+main()
