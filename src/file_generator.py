e551ddfcb579f8a91095e9e75d2885c083037402
Ruben Madelaine <madelaine.ruben@hotmail.com>
2020-10-28 23:56:36 +0100

    Applying Black formater

--- src/file_generator.py
+++ src/file_generator.py
@@ -2,8 +2,8 @@ import random
 import os
 
 
-SOURCE_NAME_DIR = 'name_list'
-NEW_FILE_DIR = 'new_files'
+SOURCE_NAME_DIR = "name_list"
+NEW_FILE_DIR = "new_files"
 FILE_EXTENSIONS = [".txt", ".md"]
 
 NB_FILES_TO_CREATE = 10
@@ -14,26 +14,31 @@ def open_file(path):
         lines = f.read().splitlines()
     return lines
 
+
 def pick_random_item(list):
-    return list[random.randint(0, len(list)-1)]
+    return list[random.randint(0, len(list) - 1)]
+
 
 def get_source_files(directory_path):
-    list=[]
+    list = []
     for root, dirs, files in os.walk(directory_path):
         for file_name in files:
             list += [file_name]
     return list
 
+
 def create_file(directory, title, extension, content):
-    file_name = os.path.join(directory, title + extension)  
-    file = open(file_name,"w")
+    file_name = os.path.join(directory, title + extension)
+    file = open(file_name, "w")
     file.write(content)
     file.close()
-    return file_name 
+    return file_name
+
 
 def create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
 
 def create_random_file_from_source_dir(source_dir, file_extensions, destination):
     source_files = get_source_files(source_dir)
@@ -41,13 +46,13 @@ def create_random_file_from_source_dir(source_dir, file_extensions, destination)
 
     file_path = os.path.join(source_dir, my_random_file)
     potential_names = open_file(file_path)
-    
+
     my_random_name = pick_random_item(potential_names).lower()
-    
+
     file_title = my_random_file[0] + "_" + my_random_name
     extension = pick_random_item(file_extensions)
     content = my_random_name
-    
+
     create_dir(destination)
     return create_file(destination, file_title, extension, content)
 
@@ -55,10 +60,12 @@ def create_random_file_from_source_dir(source_dir, file_extensions, destination)
 # -------------- Main --------------
 
 
-def main(): 
+def main():
     for i in range(NB_FILES_TO_CREATE):
-        file_name = create_random_file_from_source_dir(SOURCE_NAME_DIR, FILE_EXTENSIONS, NEW_FILE_DIR)
+        file_name = create_random_file_from_source_dir(
+            SOURCE_NAME_DIR, FILE_EXTENSIONS, NEW_FILE_DIR
+        )
         print(f"File #{i+1}: {file_name}")
-    
+
 
 main()
--- src/json_structure_analyzer.py
+++ src/json_structure_analyzer.py
@@ -3,11 +3,11 @@ import json
 import datetime
 
 
-ROOT = 'new_files'
-OUTPUT = 'analyzed_output/'
-STRUCTURE_NAME = 'analyzed_structure'
+ROOT = "new_files"
+OUTPUT = "analyzed_output/"
+STRUCTURE_NAME = "analyzed_structure"
 
-DATE_FORMAT = '%Y-%m-%d-%H:%M:%S'
+DATE_FORMAT = "%Y-%m-%d-%H:%M:%S"
 
 
 class DirEntry:
@@ -16,20 +16,24 @@ class DirEntry:
         self.path = path
         self.type = type
         self.size = size
-        self.creation_time = datetime.datetime.fromtimestamp(creation_time).strftime(DATE_FORMAT)
+        self.creation_time = datetime.datetime.fromtimestamp(creation_time).strftime(
+            DATE_FORMAT
+        )
 
     def __str__(self):
         date_time = self.creation_time
-        txt = f'{self.name} (format: {self.type}, size: {self.size} bytes, creation date: {date_time})'
+        txt = f"{self.name} (format: {self.type}, size: {self.size} bytes, creation date: {date_time})"
         return txt
+
 
 def analyze(root):
     properties = retrieve_properties(root)
 
     create_dir(OUTPUT)
     date = datetime.datetime.today().strftime(DATE_FORMAT)
-    dest_name = OUTPUT + '-'.join([STRUCTURE_NAME, root, date]) + '.json'
+    dest_name = OUTPUT + "-".join([STRUCTURE_NAME, root, date]) + ".json"
     convert_to_json(properties, dest_name)
+
 
 def retrieve_properties(path):
     properties = []
@@ -42,22 +46,22 @@ def retrieve_properties(path):
 
     return properties
 
+
 def map_entry_to_DirEntry(entry):
-    type = 'dir' if entry.is_dir() else entry.name.split('.')[-1]
+    type = "dir" if entry.is_dir() else entry.name.split(".")[-1]
     return DirEntry(
-                entry.name, 
-                entry.path, 
-                type, 
-                entry.stat().st_size,
-                entry.stat().st_ctime)
+        entry.name, entry.path, type, entry.stat().st_size, entry.stat().st_ctime
+    )
+
 
 def create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def convert_to_json(data, destination):
     serialized_data = [vars(d) for d in data]
-    with open(destination, 'w', encoding='utf-8') as f:
+    with open(destination, "w", encoding="utf-8") as f:
         json.dump(serialized_data, f, ensure_ascii=False, indent=4)
 
     print(f"File created at {destination}")
@@ -66,8 +70,8 @@ def convert_to_json(data, destination):
 # -------------- Main --------------
 
 
-def main(): 
-   analyze(ROOT)
+def main():
+    analyze(ROOT)
 
 
 main()
